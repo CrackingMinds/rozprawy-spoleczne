9b8991b70aa567744923611d47657dd0bdc196dc
Viacheslav Guselnykov <viacheslav.guselnykov@gmail.com>
2019-01-25 21:49:26 +0100

    editable editorial and scientific board

--- src/app/admin/pages/editorial-scientific-board/editorial.scientific.board.edit.component.ts
+++ src/app/admin/pages/editorial-scientific-board/editorial.scientific.board.edit.component.ts
@@ -1,19 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+
+import { Subject } from 'rxjs';
+
+import { EditorialBoardMember } from 'app/models/editorial-board-member';
 
 @Component({
 	selector: 'rs-editorial-scientific-board-edit',
 	templateUrl: `editorial.scientific.board.edit.component.html`,
   styleUrls: ['./editorial.scientific.board.edit.component.scss']
 })
-export class EditorialScientificBoardEditComponent implements OnInit {
+export class EditorialScientificBoardEditComponent implements OnInit, OnDestroy {
 
-	constructor() {
+  form: FormGroup = this.formBuilder.group({
+    editorialBoardMembers: this.formBuilder.array([])
+  });
 
-	}
+  get editorialBoardMembers(): FormArray {
+    return this.form.get('editorialBoardMembers') as FormArray;
+  }
+
+  private unsubscribe$: Subject<void> = new Subject<void>();
+
+  constructor(private formBuilder: FormBuilder) {}
 
 	ngOnInit() {
-
+    this.addEmptyEditorialBoardMemberControl();
 	}
 
+	ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+	addEmptyEditorialBoardMemberControl(): void {
+    const boardMember: EditorialBoardMember = {
+      id: undefined,
+      position: undefined,
+      firstName: undefined,
+      lastName: undefined,
+      middleName: undefined
+    };
+
+    this.addEditorialBoardMemberControl(boardMember);
+  }
+
+	addEditorialBoardMemberControl(boardMember: EditorialBoardMember): void {
+    this.editorialBoardMembers.push(
+      this.formBuilder.control(boardMember, [
+        Validators.required
+      ])
+    );
+  }
+
+  removeEditorialBoardMember(index: number): void {
+    this.editorialBoardMembers.removeAt(index);
+  }
 
 }
--- src/app/admin/pages/editorial-scientific-board/editorial.scientific.board.edit.module.ts
+++ src/app/admin/pages/editorial-scientific-board/editorial.scientific.board.edit.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatIconModule } from '@angular/material';
 
 import { EditorialScientificBoardEditComponent } from 'app/admin/pages/editorial-scientific-board/editorial.scientific.board.edit.component';
+import { EditorialBoardMemberControlModule } from 'app/shared/form-controls/editorial-board-member/editorial.board.member.control.module';
 
 const declarations = [
   EditorialScientificBoardEditComponent
@@ -11,10 +15,15 @@ const declarations = [
 @NgModule({
 	imports: [
 		CommonModule,
+    ReactiveFormsModule,
     RouterModule.forChild([{
       path: '',
       component: EditorialScientificBoardEditComponent
-    }])
+    }]),
+
+    MatIconModule,
+
+    EditorialBoardMemberControlModule
 	],
 	declarations: declarations,
 	exports: declarations
