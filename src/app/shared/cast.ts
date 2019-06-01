9284a6aa13cda7dcb6958e0d7d547d905cad5e95
Viacheslav Guselnykov <viacheslav.guselnykov@gmail.com>
2019-06-01 13:23:41 +0200

    Person Cast Fix

--- src/app/shared/cast.ts
+++ src/app/shared/cast.ts
@@ -3,6 +3,9 @@ import { Person } from 'app/models/person';
 export class Cast {
 
   static toPerson(value: any): Person {
+    if (!value)
+      return null;
+
     return {
       firstName: value.firstName === "" ? null : value.firstName,
       lastName: value.lastName === "" ? null : value.lastName,
